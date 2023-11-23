da3c17b2a3ece4f8a8017c830a2cc102fdbfbbf1
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-23 11:16:43 -0600

    modified:   Project_2/app.js

--- Project_2/app.js
+++ Project_2/app.js
@@ -13,5 +13,19 @@ console.log(galleryModal);
 imgGallery.forEach((img) => {
   img.addEventListener('click', () => {
     galleryModal.classList.add('open');
+    fullImage.classList.add('open');
+    //Getting the image alt attribute dynamically
+    const imgAttribute = img.getAttribute('alt');
+    //Getting the src of each image dynamically
+    fullImage.src = `./img/full/${imgAttribute}.jpg`;
   });
+});
+
+galleryModal.addEventListener('click', (e) => {
+  if (e.target.classList.contains('modal')) {
+    //Remove Modal window that contains Class Open
+    galleryModal.classList.remove('open');
+    // Remove Full Image Window that contains Class Open & animate
+    fullImage.classList.remove('open');
+  }
 });
