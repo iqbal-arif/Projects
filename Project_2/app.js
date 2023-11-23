e2a74961349aa71aa0e5716750ba33964f39528a
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-23 10:30:51 -0600

    modified:   Project_2/app.js

--- Project_2/app.js
+++ Project_2/app.js
@@ -1,13 +1,18 @@
-const gallerySection = document.querySelector('.gallery');
-console.log(gallerySection);
 // const galleryImage = document.querySelector('.gallery');
 // console.log(galleryImage.children);
 const imageContainer = document.querySelectorAll('.img-container').src;
 console.log(imageContainer);
-const galleryModal = document.querySelector('.modal');
-// console.log(galleryModal);
+/******************** */
+//Full image selector
 const fullImage = document.querySelector('.full-img');
 console.log(fullImage.alt);
+//Gallery section that has all the small images
+const gallerySection = document.querySelector('.gallery');
+console.log(gallerySection);
+//Modal to show large images
+const galleryModal = document.querySelector('.modal');
+console.log(galleryModal);
+const smallImage = document.querySelector('.small-img');
 
 // for (const img of imageContainer) {
 //   img.addEventListener('click', (e) => {
