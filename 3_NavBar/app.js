8f6b71703a911386481f93eb5a36de11902433a9
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-29 13:33:20 -0600

    modified:   3_NavBar/app.js modified:   3_NavBar/style.css modified:   3_NavBar/app.js modified:   3_NavBar/index.html 4_Midi-Keyboard/

---
+++ 3_NavBar/app.js
@@ -0,0 +1,5 @@
+//Intersection Observer Api Practical Example
+https://medium.com/@ryanfinni/the-intersection-observer-api-practical-examples-7844dfa429e9
+
+https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+
