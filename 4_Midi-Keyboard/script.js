f8d78a0ff981d1191ae7e73079239c8581657327
Arif Iqbal <arif_iqbal@yahoo.com>
2023-11-29 14:41:53 -0600

    modified:   4_Midi-Keyboard/script.js

--- 4_Midi-Keyboard/script.js
+++ 4_Midi-Keyboard/script.js
@@ -1,16 +1,16 @@
 const NOTE_DETAILS = [
-  { note: 'C', key: 'Z', frequency: 261.626 },
-  { note: 'Db', key: 'S', frequency: 277.183 },
-  { note: 'D', key: 'X', frequency: 293.665 },
-  { note: 'Eb', key: 'D', frequency: 311.127 },
-  { note: 'E', key: 'C', frequency: 329.628 },
-  { note: 'F', key: 'V', frequency: 349.228 },
-  { note: 'Gb', key: 'G', frequency: 369.994 },
-  { note: 'G', key: 'B', frequency: 391.995 },
-  { note: 'Ab', key: 'H', frequency: 415.305 },
-  { note: 'A', key: 'N', frequency: 440 },
-  { note: 'Bb', key: 'J', frequency: 466.164 },
-  { note: 'B', key: 'M', frequency: 493.883 },
+  { note: 'C', key: 'Z', frequency: 261.626, active: false },
+  { note: 'Db', key: 'S', frequency: 277.183, active: false },
+  { note: 'D', key: 'X', frequency: 293.665, active: false },
+  { note: 'Eb', key: 'D', frequency: 311.127, active: false },
+  { note: 'E', key: 'C', frequency: 329.628, active: false },
+  { note: 'F', key: 'V', frequency: 349.228, active: false },
+  { note: 'Gb', key: 'G', frequency: 369.994, active: false },
+  { note: 'G', key: 'B', frequency: 391.995, active: false },
+  { note: 'Ab', key: 'H', frequency: 415.305, active: false },
+  { note: 'A', key: 'N', frequency: 440, active: false },
+  { note: 'Bb', key: 'J', frequency: 466.164, active: false },
+  { note: 'B', key: 'M', frequency: 493.883, active: false },
 ];
 
 const keyDisplay = document.querySelectorAll('[data-note]');
@@ -43,5 +43,8 @@ function getNoteDetail(keyBoardCode) {
 }
 
 function playNotes() {
-  console.log('play Notes');
+  const keyElement = NOTE_DETAILS.forEach((keyDisplay) =>
+    console.log(keyDisplay)
+  );
+  console.log(keyElement);
 }
