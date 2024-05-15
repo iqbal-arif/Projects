b068fa4ee2b6ab9d016d22af612c9be775378416
Arif Iqbal <arif_iqbal@yahoo.com>
2024-05-14 22:32:12 -0500

    Updated js files

--- Project_3_CheckBox/checkUncheckAllCheckbox.js
+++ Project_3_CheckBox/checkUncheckAllCheckbox.js
@@ -17,3 +17,43 @@ function uncheckAll() {
 
 const btn = document.querySelector('#btn');
 btn.onclick = checkAll;
+
+/*
+How it works:
+
+First, define the check() function that checks or unchecks all checkboxes with the name "color":
+
+function check(checked = true) {
+  const checkboxes = document.querySelectorAll('input[name="color"]');
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = checked;
+  });
+}
+Code language: JavaScript (javascript)
+
+When you click the button, it checked all the checkboxes. And. If you click the button again, it should uncheck all the checkboxes. To do this switch, you need to reassign the click event handler whenever the click event fires.
+
+Second, select the #btn button and assign the checkAll() function to the onclick property of the button:
+
+const btn = document.querySelector('#btn');
+btn.onclick = checkAll;
+Code language: JavaScript (javascript)
+
+Third, define the checkAll() function that checks all the checkboxes:
+
+function checkAll() {
+  check();
+  this.onclick = uncheckAll;
+}
+Code language: JavaScript (javascript)
+
+Finally, define the uncheckAll() function that unchecks all the checkboxes:
+
+function uncheckAll() {
+  check(false);
+  this.onclick = checkAll;
+}
+
+
+
+*/
--- Project_3_CheckBox/getMultiCheckboxValues.js
+++ Project_3_CheckBox/getMultiCheckboxValues.js
@@ -7,3 +7,40 @@ btn.addEventListener('click', (event) => {
   });
   alert(values);
 });
+
+/*
+How it works.
+
+In the HTML, we create three checkbox elements with the same name (color) but distinct values:
+
+<label for="c1"><input type="checkbox" name="color" value="red" id="c1">Red</label>
+<label for="c2"><input type="checkbox" name="color" value="green" id="c2">Green</label>
+<label for="c3"><input type="checkbox" name="color" value="blue" id="c3">Blue</label>
+Code language: HTML, XML (xml)
+
+In the JavaScript:
+
+First, add the click event handler to the button:
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', (event) => {
+   // ...            
+});    
+Code language: JavaScript (javascript)
+
+Second, select the selected checkboxes using the document.querySelectorAll() method inside the click event handler:
+
+let checkboxes = document.querySelectorAll('input[name="color"]:checked');
+Code language: JavaScript (javascript)
+
+Third, push the values of the selected checkboxes to an array:
+
+let values = [];
+checkboxes.forEach((checkbox) => {
+    values.push(checkbox.value);
+});
+
+alert(values);
+
+
+*/
