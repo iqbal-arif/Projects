4f516cad1f8634e7cc76024d6c861725be724c41
Arif Iqbal <arif_iqbal@yahoo.com>
2024-06-03 22:48:50 -0500

    Added Get Element Style from JS

---
+++ Project_16_GetElementStyle/getElementStyle.js
@@ -0,0 +1,11 @@
+/*
+Get Styles of an Element
+
+To get the styles of an element, you use the window.getComputedStyle() method:
+*/
+let style = window.getComputedStyle(element [,pseudoElement]);
+/*
+Code language: JavaScript (javascript)
+
+The getComputedStyle() method returns a style object that represents the current active styles of an element.
+*/
