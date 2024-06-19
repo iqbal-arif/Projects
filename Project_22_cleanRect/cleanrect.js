926a4cce5177eeb0cb6b4d25c05b2d7b15fe0cd3
Arif Iqbal <arif_iqbal@yahoo.com>
2024-06-19 12:32:09 -0500

    Added cleanrect

---
+++ Project_22_cleanRect/cleanrect.js
@@ -0,0 +1,21 @@
+function draw() {
+  const canvas = document.querySelector('#canvas');
+
+  if (!canvas.getContext) {
+    return;
+  }
+
+  const ctx = canvas.getContext('2d');
+
+  // draw two squares
+  ctx.fillStyle = '#F9DC5C';
+  ctx.fillRect(50, 50, 150, 150);
+
+  ctx.fillStyle = 'rgba(0,0,255,0.5)';
+  ctx.fillRect(100, 100, 150, 150);
+
+  // clear the intersection
+  ctx.clearRect(100, 100, 100, 100);
+}
+
+draw();
