092f4940a8d79c4a7aefbcbed62371a4680b845a
Arif Iqbal <arif_iqbal@yahoo.com>
2024-07-30 12:58:45 -0500

    commented arc.js

--- Project_23_Arc/arc.js
+++ Project_23_Arc/arc.js
@@ -1,13 +1,13 @@
-// Canvas Arc JavaScript 
+// Canvas Arc JavaScript
 const canvas = document.querySelector('#canvas');
 
 if (canvas.getContext) {
   const ctx = canvas.getContext('2d');
-
+  /*
   ctx.strokeStyle = 'red';
   ctx.fillStyle = 'rgba(255,0,0,0.1)';
   ctx.lineWidth = 5;
-
+*/
   ctx.beginPath();
   ctx.arc(canvas.width / 2, canvas.height / 2, 100, 0, 2 * Math.PI);
 
