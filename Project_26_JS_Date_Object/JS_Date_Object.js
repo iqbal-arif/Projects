923efd4b10d4332d16c0236eb4deb6d5f9c3de4b
Arif Iqbal <arif_iqbal@yahoo.com>
2024-08-02 12:58:43 -0500

    Updated Date

--- Project_26_JS_Date_Object/JS_Date_Object.js
+++ Project_26_JS_Date_Object/JS_Date_Object.js
@@ -34,3 +34,27 @@ console.log(date3); // Wed Jul 01 2020 00:00:00 GMT+0545
 
 const date4 = new Date('JULY, 1, 2020');
 console.log(date4); // Wed Jul 01 2020 00:00:00
+
+new Date(year, month, day, hours, minutes, seconds, milliseconds)
+
+new Date(year, month,...) creates a new date object by passing specific date and time. For example,
+
+const time1 = new Date(2020, 1, 20, 4, 12, 11, 0);
+console.log(time1); // Thu Feb 20 2020 04:12:11
+/*
+The passed argument has a specific order.
+
+If four numbers are passed, it represents year, month, day and hours. For example,
+*/
+const time1 = new Date(2020, 1, 20, 4);
+console.log(time1); // Thu Feb 20 2020 04:00:00
+/*
+Similarly, if two arguments are passed, it represents year and month. For example,
+*/
+const time1 = new Date(2020, 1);
+console.log(time1); // Sat Feb 01 2020 00:00:00
+/*
+Note: If you pass only one argument, it is treated as milliseconds. Hence, you have to pass two arguments to use this date format.
+
+In JavaScript, months are counted from 0 to 11. January is 0 and December is 11.
+*/
