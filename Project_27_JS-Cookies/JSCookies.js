0d144a3cef2887c53f91e5cbe54df12736cdac78
Arif Iqbal <arif_iqbal@yahoo.com>
2024-08-05 13:11:54 -0500

    added js cookies

---
+++ Project_27_JS-Cookies/JSCookies.js
@@ -0,0 +1,15 @@
+/*Cookies in JavaScript
+
+To manage cookies in JavaScript, you use the document.cookie property.
+1) Get a cookie value
+
+The following example returns a string of all cookies available to the page:
+*/
+const str = document.cookie;
+/*
+Code language: JavaScript (javascript)
+
+The document.cookie returns a series of name-value pairs separated by semicolons like this:
+*/
+name1 = value1;
+name2 = value2;
