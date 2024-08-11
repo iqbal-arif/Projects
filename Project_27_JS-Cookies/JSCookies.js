617449de43fe9b54f8a10fef64a31b34d62d003c
Arif Iqbal <arif_iqbal@yahoo.com>
2024-08-11 12:55:21 -0500

    remoVe cookie added

--- Project_27_JS-Cookies/JSCookies.js
+++ Project_27_JS-Cookies/JSCookies.js
@@ -57,4 +57,44 @@ document.cookie = `${encodeURIComponent('username')}=${encodeURIComponent(
 3) Remove a cookie
 
 To remove a cookie, you need to set the cookie again with the same name, path, domain, and secure option. And you need to set the expiration date to some time in the past.
+
+JavaScript Cookie class
+
+The following Cookie class sets, gets, and removes a cookie:
 */
+class Cookie {
+  static get(name) {
+    const cookieName = `${encodeURIComponent(name)}=`;
+    const cookie = document.cookie;
+    let value = null;
+
+    const startIndex = cookie.indexOf(cookieName);
+    if (startIndex > -1) {
+      const endIndex = cookie.indexOf(';', startIndex);
+      if (endIndex == -1) {
+        endIndex = cookie.length;
+      }
+      value = decodeURIComponent(
+        cookie.substring(startIndex + name.length, endIndex)
+      );
+    }
+    return value;
+  }
+
+  static set(name, value, expires, path, domain, secure) {
+    let cookieText = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
+    if (expires instanceof Date) {
+      cookieText += `; expires=${expires.toGMTString()}`;
+    }
+
+    if (path) cookieText += `; path=${path}`;
+    if (domain) cookieText += `; domain=${domain}`;
+    if (secure) cookieText += `; secure`;
+
+    document.cookie = cookieText;
+  }
+
+  static remove(name, path, domain, secure) {
+    Cookie.set(name, '', new Date(0), path, domain, secure);
+  }
+}
