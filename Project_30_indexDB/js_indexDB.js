1b6ba1966e38aac332670eaa91f373bbce96112e
Arif Iqbal <arif_iqbal@yahoo.com>
2024-09-04 14:14:21 -0500

    update session

--- Project_30_indexDB/js_indexDB.js
+++ Project_30_indexDB/js_indexDB.js
@@ -270,3 +270,60 @@ Now, if you open the index.html file in the web browser, the code in the app.js 
 
 If you open the devtools on the web browser, you’ll see the CRM database with the Contacts object store. And in the Contacts object store, you’ll see the data there as shown in the following picture:
 */
+/*
+5) Read data from the object store by key
+
+To read an object by its key, you use the get() method of the object store. The following getContactById() function finds a contact by an id:
+*/
+function getContactById(db, id) {
+  const txn = db.transaction('Contacts', 'readonly');
+  const store = txn.objectStore('Contacts');
+
+  let query = store.get(id);
+
+  query.onsuccess = (event) => {
+    if (!event.target.result) {
+      console.log(`The contact with ${id} not found`);
+    } else {
+      console.table(event.target.result);
+    }
+  };
+
+  query.onerror = (event) => {
+    console.log(event.target.errorCode);
+  };
+
+  txn.oncomplete = function () {
+    db.close();
+  };
+}
+/*
+Code language: JavaScript (javascript)
+
+When you call the get() method of the object store, it returns a query that will execute asynchronously.
+
+Because the query can succeed or fail, you need to assign the onsuccess and onerror handlers to handle each case.
+
+If the query succeeded, you’ll get the result in the event.target.result. Otherwise, you’ll get an error code via event.target.errorCode.
+
+The following code closes the connection to the database once the transaction completes:
+*/
+txn.oncomplete = function () {
+  db.close();
+};
+/*
+Code language: JavaScript (javascript)
+
+Actually, the database connection is closed only when all the transactions are completed.
+
+The following calls the getContactById() in the onsuccess event handler to get the contact with id 1:
+*/
+request.onsuccess = (event) => {
+  const db = event.target.result;
+  getContactById(db, 1);
+};
+/*
+Code language: JavaScript (javascript)
+
+Output:
+*/
